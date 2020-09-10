3c47b6271657f8bf08e73ce988ad2d5cd5f448e0
Natan Bassani <natan_bassani@hotmail.com>
2020-09-10 09:10:23 -0400

    Update file.php

--- file.php
+++ file.php
@@ -1,22 +1,25 @@
-<html>
-<a onclick="copyToClipboard('EXEMPLO')">
-    <span style="cursor:pointer; background-color:#fafbfc;padding:20px;">
-    EXEMPLO
-    </span>
-</a>
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<body>
+		<a onclick="copyToClipboard('EXEMPLO')">
+		    <span style="cursor:pointer; background-color:#fafbfc;padding:20px;">
+		    EXEMPLO
+		    </span>
+		</a>
 
-<script type="text/javascript">
-function copyToClipboard(value) {
-	
-	var tempInput = document.createElement("input");
-    tempInput.style = "position: absolute; left: -1000px; top: -1000px";
-    tempInput.value = value;
-    document.body.appendChild(tempInput);
-    tempInput.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempInput);
-	
-	alert('Copiado: '+tempInput.value);
-}
-</script>
+		<script type="text/javascript">
+		function copyToClipboard(value) {
+
+			var tempInput = document.createElement("input");
+		    tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+		    tempInput.value = value;
+		    document.body.appendChild(tempInput);
+		    tempInput.select();
+		    document.execCommand("copy");
+		    document.body.removeChild(tempInput);
+
+			alert('Copiado: '+tempInput.value);
+		}
+		</script>
+	</body>
 </html>
