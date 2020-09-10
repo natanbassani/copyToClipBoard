1b78a5a6e94268ceb15f9c4ac13ddfd86a2481d2
Natan Bassani <natan_bassani@hotmail.com>
2020-09-10 09:08:29 -0400

    Create file.php

---
+++ file.php
@@ -0,0 +1,22 @@
+<hml>
+<a onclick="copyToClipboard('EXEMPLO')">
+    <span style="cursor:pointer; background-color:#fafbfc;padding:20px;">
+    EXEMPLO
+    </span>
+</a>
+
+<script type="text/javascript">
+function copyToClipboard(value) {
+	
+	var tempInput = document.createElement("input");
+    tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+    tempInput.value = value;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+	
+	alert('Copiado: '+tempInput.value);
+}
+</script>
+</html>
